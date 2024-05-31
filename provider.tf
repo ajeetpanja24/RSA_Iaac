f1c3b5d32a988d2fad182da21e051c8e0375d3aa
ajeetpanja24 <ajeetpanja24@gmail.com>
2024-05-31 23:48:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "84fc6d5d-6dc9-459d-b1af-715b14bec9ad"
-  tenant_id       = "2a451167-efd5-4dcb-9679-5809757c2464"
-  client_id       = "9bee6144-c866-45e0-a5ff-98991a9775a3"
-  client_secret   = "ydv8Q~zgs42w8HeqWsVrnY6aqhwnuCP1zE4QQaQY"
+  subscription_id = "84fc6d5d-&&&&&&&&&&" ### These are the dummy Vaules 
+  tenant_id       = "2a45***************c2464"
+  client_id       = "9b@##$$$@##$%^&&5a3"
+  client_secret   = "ydv8!@#$%^&*UYTRHGFD"
   features {}
 }
